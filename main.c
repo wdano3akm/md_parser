9d30db57844db5d98b85e30fe276e323425d27e6
Gianmaria Piergianni <g.piergianni@student.tudelft.nl>
2025-05-23 01:49:15 +0200

    integrated functionality with reader and writer, wrote makefile, still a bug to fix

--- main.c
+++ main.c
@@ -1,4 +1,21 @@
-int main(){
-	
+#include <stdlib.h>
+#include <stdio.h>
+#include "reader.h"
+#include "parser.h"
+#include "writer.h"
+int main(int argc, char** argv){
+	if (argc < 2){
+		fprintf(stderr, "what am i supposed to parse if you don't input a file????");
+		exit(1);
+	}
+
+	char* file_contents = read_file(argv[1]);
+	char* output = parse(file_contents);
+	if (argc == 2) printf("%s", output);
+	else if (argc == 3) writer(output, argv[2]); 
+	else {
+		fprintf(stderr, "Too many arguments used");
+		exit(1);
+	}
 	return 0;
 }
--- reader.c
+++ reader.c
@@ -1,39 +1,67 @@
+//#include <cstdlib>
 #include <stdio.h>
+#include <string.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "parser.h"
+#include <unistd.h>
 
-char* read_file(char file_path[]){
-	FILE *fp;	
+
+int check_path(char*);
+
+char* read_file(char* file_path){
+	int fildes;
 	struct stat buffer;
 
-
-	fp = fopen(file_path, "r");
-	if (!fp) {
-		perror("fopen");
-		return NULL;
-	}
-	int fildes = open(file_path, O_RDWR);
+	fildes = check_path(file_path);
 	int status = fstat(fildes ,&buffer);
-	char* read_chars = malloc(buffer.st_size);
+	char* read_chars = malloc(buffer.st_size );
 
-
-	size_t ret = fread(read_chars,sizeof(*read_chars), buffer.st_size,fp);
+	
+	size_t ret = read(fildes, read_chars, buffer.st_size );
 	if (ret != buffer.st_size){
 		fprintf(stderr, "oh fuck, read: %zu\nread_charser: %zu", ret, buffer.st_size);
 		return NULL;
 	}
+	
 
-	//	printf("%s",read_chars );
 	return read_chars;
 
 }
+
+int check_path(char* file_path){
+	int fd;
+	if (file_path[0] == '/'){
+		fd = open(file_path, O_RDONLY);
+	} else {
+		char* tmp = malloc(strlen(file_path) + 3);
+		sprintf(tmp, "./%s", file_path);
+		file_path = tmp;
+		fd = open(file_path, O_RDONLY);
+//		printf("%s", file_path);
+//		printf("%d", fd);
+	};
+
+	if (fd < 0){
+		fprintf(stderr, "can't find the file\n");
+		exit(1);
+	}
+	return fd;
+}
+
 /*
-int main(){
-	read_file("test.txt");
+int main(int argc, char **argv){
+
+	if (argc < 2) {
+		fprintf(stderr, "please provide file\n");
+		exit(1);
+
+	}
+	char* string = read_file(argv[1]);
+	printf("%s", string);
+
 	return 0;
 }
 */
